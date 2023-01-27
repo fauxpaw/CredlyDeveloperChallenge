6dd631d9ea1f581b204d07a8dd73576c73b5f69d
Fauxpaw <fauxpawsc@live.com>
2023-01-26 17:17:55 -0800

    create xcode project

---
+++ CredlyDeveloperChallenge/CredlyDeveloperChallenge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CredlyDeveloperChallenge
+//
+//  Created by Michael Sweeney on 1/26/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
