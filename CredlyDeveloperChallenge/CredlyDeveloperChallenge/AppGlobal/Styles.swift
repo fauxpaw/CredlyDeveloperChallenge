ecba9a95caeedef2a4db1a9d74406c59d5e9dc8b
Fauxpaw <fauxpawsc@live.com>
2023-01-29 20:20:33 -0800

    add formatting for names and phonenumbers, add brand colors

---
+++ CredlyDeveloperChallenge/CredlyDeveloperChallenge/AppGlobal/Styles.swift
@@ -0,0 +1,17 @@
+//
+//  Styles.swift
+//  CredlyDeveloperChallenge
+//
+//  Created by Michael Sweeney on 1/29/23.
+//
+
+import UIKit
+
+struct Color {
+    static let primaryBackground = #colorLiteral(red: 0.4688314199, green: 0.7923993468, blue: 0.7880969644, alpha: 1)
+    static let secondaryBackground = #colorLiteral(red: 0.9761696458, green: 0.833968699, blue: 0.4549742341, alpha: 1)
+    static let primaryHighlight = #colorLiteral(red: 0, green: 0.525767386, blue: 0.4914978147, alpha: 1)
+    static let secondaryHighlight = #colorLiteral(red: 0.9959205985, green: 0.5585259795, blue: 0.08471687883, alpha: 1)
+}
+
+
--- CredlyDeveloperChallenge/CredlyDeveloperChallenge/Features/ContactsList/UserContactTableViewCell.swift
+++ CredlyDeveloperChallenge/CredlyDeveloperChallenge/Features/ContactsList/UserContactTableViewCell.swift
@@ -9,21 +9,21 @@ import UIKit
 
 class UserContactTableViewCell: UITableViewCell {
 
-    
     @IBOutlet weak var initialsLabel: UILabel!
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var emailLabel: UILabel!
     @IBOutlet weak var phoneNumberLabel: UILabel!
     
+    private let circularRadius: CGFloat = 24
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        initialsLabel.layer.masksToBounds = true
+        initialsLabel.layer.cornerRadius = circularRadius
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
     }
 
 }
